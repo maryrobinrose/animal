c742c7e363f6c3f5ffba1aece91d4ec3f331baa7
Alex Garcia and Mary McMurray <mary@>
2018-10-18 13:11:05 -0700

    add .change function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,31 @@
 $(function(){
-  $("#snuffles").click(function(event) {
-    $("#catInfo").toggle();
-    $(event).eventDefault();
-  });
-  $("#dogs").click(function(event) {
-    $("#dogInfo").toggle();
-    $(event).eventDefault();
-  });
+
+  $("#bar").change(function(){
+    var animal = $("#bar").val();
+    console.log(animal)
+
+      if (animal = "val.cats") {
+        $("#catInfo").show()
+      }
+
+
+   });
 });
+
+
+
+    // $(function message() {
+    //
+    //   var s = document.getElementById('#animals');
+    //   var #animals = s.options[s.selectedIndex]".value";
+
+      // if ("#animals" === 1) {
+      //     .show("#catInfo")
+      // }
+      // else if ("#animals" === 2) {
+      //     alert("it equals microphone")
+      // }
+      // else if ("#animals" == 3) {
+      //     alert("it equals tripod")
+    //   }
+    // });
